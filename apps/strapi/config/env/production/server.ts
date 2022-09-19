fb2630dc2aee92d80263edbfd860f56814c033f7
dominikjasek <dominik.jasek@customink.com>
2022-09-19 11:23:03 +0200

    persist db - render persistent disk is at the root of project

--- apps/strapi/config/env/production/server.ts
+++ apps/strapi/config/env/production/server.ts
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL"),
+  url: env('RENDER_EXTERNAL_URL'),
   dirs: {
-    public: "/data/public"
+    public: '/data/public'
   },
 });
--- apps/strapi/production/dump-db.sh
+++ apps/strapi/production/dump-db.sh
@@ -3,4 +3,4 @@
 #This requires you to add ssh public key to render (https://render.com/docs/ssh-keys)
 
 mkdir -p db-dumps
-scp srv-cci6impa6gdiindmkeo0@ssh.frankfurt.render.com:/opt/render/project/src/apps/strapi/data/strapi.db ./db-dumps/strapi-db-"$(date +%d-%m-%Y_%H:%M)".db
+scp srv-cci6impa6gdiindmkeo0@ssh.frankfurt.render.com:/data/strapi.db ./db-dumps/strapi-db-"$(date +%d-%m-%Y_%H:%M)".db
