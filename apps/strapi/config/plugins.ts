734b1720aa9c9ef702c588236df43eae6e166076
dominikjasek <dominik.jasek@customink.com>
2022-09-21 22:50:19 +0200

    strapi send emails using sendinblue service

--- apps/strapi/config/plugins.ts
+++ apps/strapi/config/plugins.ts
@@ -1,4 +1,16 @@
 module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'strapi-provider-email-sendinblue',
+      providerOptions: {
+        sendinblue_api_key: env('SIB_API_KEY', 'xkeysib-0987654321-abcdef'),
+        sendinblue_default_replyto: env('SIB_DEFAULT_REPLY_TO', 'contact@example.com'),
+        sendinblue_default_from: env('SIB_DEFAULT_FROM', 'no-reply@example.com'),
+        sendinblue_default_from_name: env('SIB_DEFAULT_FROM_NAME', 'Sender Name'),
+      },
+    },
+  },
+
   upload: {
     config: {
       provider: 'aws-s3',
