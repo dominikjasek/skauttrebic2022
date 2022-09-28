fe35997ea98c6bad0b8da909c83113652d433423
dominikjasek <dominik.jasek@customink.com>
2022-09-28 20:32:50 +0200

    init strapi config sync

--- apps/strapi/config/admin.ts
+++ apps/strapi/config/admin.ts
@@ -1,8 +1,11 @@
-export default ({ env }) => ({
+export default ({env}) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
+  watchIgnoreFiles: [
+    '**/config/sync/**',
+  ],
 });
