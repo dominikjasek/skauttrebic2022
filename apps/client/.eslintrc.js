b1b736dc359cb58ed074a1255bccb0a396c2d5a5
dominikjasek <dominik.jasek@customink.com>
2022-10-01 21:02:47 +0200

    add react-use

--- apps/client/.eslintrc.js
+++ apps/client/.eslintrc.js
@@ -1,34 +1,35 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    'overrides': [
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react',
-        '@typescript-eslint'
-    ],
-    'rules': {
-        '@typescript-eslint/semi': ['error', 'never'],
-        'react/react-in-jsx-scope': 'off',
-        'object-curly-spacing': ['error', 'always'],
-        'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1 }],
-        'react/jsx-space-before-closing': ['error', 'always'],
-        'react/jsx-indent' : ['error', 4],
-        'quotes': ['error', 'single'],
-        '@typescript-eslint/ban-ts-comment': 'off',
-        'no-trailing-spaces': 'error'
-    }
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    '@typescript-eslint/semi': ['error', 'never'],
+    'react/react-in-jsx-scope': 'off',
+    'object-curly-spacing': ['error', 'always'],
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1 }],
+    'react/jsx-space-before-closing': ['error', 'always'],
+    'indent': ['error', 2],
+    'react/jsx-indent' : ['error', 2],
+    'quotes': ['error', 'single'],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-trailing-spaces': 'error'
+  }
 }
--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import { useTitle } from 'react-use'
 
 const Home: NextPage = () => {
-    useH
+  useTitle('Skaut Třebíč')
 
   return (
-      <main>
-          vitej doma
-          <Link href={'/testik'}>testik</Link>
-      </main>
+    <main>
+      vitej doma
+      <Link href={'/testik'}>testik</Link>
+    </main>
   )
 }
 
