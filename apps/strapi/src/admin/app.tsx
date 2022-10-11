19c2ba81ecdfa0d4a71977cc77ae618ad17b02af
dominikjasek <dominik.jasek@customink.com>
2022-10-11 10:43:18 +0200

    customize strapi admin theme and images

--- apps/strapi/src/admin/app.tsx
+++ apps/strapi/src/admin/app.tsx
@@ -1,5 +1,29 @@
+// @ts-ignore
+import SkautLogoIco from './extensions/skaut-logo.ico'
+// @ts-ignore
+import SkautLogoSvg from './extensions/skaut-logo.svg'
+
 export default {
   config: {
+    menu: {
+      logo: SkautLogoSvg,
+    },
+    head: {
+      favicon: SkautLogoIco,
+    },
+    auth: {
+      logo: SkautLogoSvg,
+    },
+    theme: {
+      colors: {
+        alternative100: '#ecfcec',
+        alternative200: '#F9B200',
+        alternative500: '#F49E00',
+        alternative600: '#3979B5',
+        alternative700: '#294885',
+        danger700: '#b72b1a'
+      },
+    },
     locales: ['cs', 'en'],
     translations: {
       cs: {
