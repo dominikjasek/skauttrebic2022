c73ad38a3fc8e8d7a9b24e32607e2ee36b86e5ca
dominikjasek <dominik.jasek@customink.com>
2022-10-25 12:48:38 +0200

    lifecycle event on user created

--- apps/strapi/src/extensions/users-permissions/strapi-server.ts
+++ apps/strapi/src/extensions/users-permissions/strapi-server.ts
@@ -9,5 +9,36 @@ module.exports = (plugin) => {
     await originalCreate(ctx)
   }
 
+  console.log(plugin)
+
+
+  plugin.bootstrap = async ({ strapi }) => {
+    strapi.db.lifecycles.subscribe({
+      models: ['plugin::users-permissions.user'],
+      async afterCreate(event) {
+        // Send email to user with reset password
+        console.log(event.params)
+      },
+
+      afterCount(): void {return},
+      afterCreateMany(): void {return},
+      afterDelete(): void {return},
+      afterDeleteMany(): void {return},
+      afterFindMany(): void {return},
+      afterFindOne(): void {return},
+      afterUpdate(): void {return},
+      afterUpdateMany(): void {return},
+      beforeCount(): void {return},
+      beforeCreate(): void {return},
+      beforeCreateMany(): void {return},
+      beforeDelete(): void {return},
+      beforeDeleteMany(): void {return},
+      beforeFindMany(): void {return},
+      beforeFindOne(): void {return},
+      beforeUpdate(): void {return},
+      beforeUpdateMany(): void {return}
+    });
+  }
+
   return plugin
 }
