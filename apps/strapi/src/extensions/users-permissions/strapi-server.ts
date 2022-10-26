c998582ee1d01a11747d104b8ba00669e72ecaa8
dominikjasek <dominik.jasek@customink.com>
2022-10-26 06:29:12 +0200

    generate random hash

--- apps/strapi/src/extensions/users-permissions/strapi-server.ts
+++ apps/strapi/src/extensions/users-permissions/strapi-server.ts
@@ -1,10 +1,12 @@
-'use strict';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const crypto = require('crypto')
 
 import { AfterXXXEvent } from '../../api/post/content-types/post/interfaces';
 
 module.exports = (plugin) => {
+  console.log(plugin.controllers.auth.resetPassword)
+
   const originalCreate = plugin.controllers.contentmanageruser.create
-  console.log(plugin)
   plugin.controllers.contentmanageruser.create = async (ctx) => {
     ctx.request.body.username = ctx.request.body.email // we don't want to work with username, but strapi somehow requires this field required, therefore we set some value
     ctx.request.body.password = 'Abcd1234'
@@ -20,7 +22,12 @@ module.exports = (plugin) => {
         console.log('result', event.result)
         const user = await strapi.entityService.findOne('plugin::users-permissions.user', event.result.id)
         console.log(user)
-        //
+
+        const hash = crypto.createHash('sha256').digest('hex').toString();
+        console.log('hash', hash)
+
+        // const result = await plugin.controllers.auth.resetPassword(user.id)
+        // console.log('result', result)
         // console.log(event.params.data.email)
         // console.log(`email: ${event.params.data.email}`)
         // await sendEmailToNewUser()
