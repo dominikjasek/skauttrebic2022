b3d50e16eb706838f6954d95f9c97ba33a710b0f
dominikjasek <dominik.jasek@customink.com>
2022-10-31 06:20:24 +0100

    make dots smaller in image slider

--- apps/client/components/Homepage/HomepageImageSlider/carousel/dots.tsx
+++ apps/client/components/Homepage/HomepageImageSlider/carousel/dots.tsx
@@ -9,8 +9,8 @@ const dotWrapStyle: React.CSSProperties = {
 }
 
 const dotItemStyle: React.CSSProperties = {
-  width: '20px',
-  height: '20px',
+  width: '12px',
+  height: '12px',
   borderRadius: '50%',
   margin: '0 10px',
   display: 'inline-block',
@@ -26,7 +26,8 @@ const Dots = ({ length, activeIndex, setActiveIndex }: DotProps) => {
           key={i}
           style={{
             ...dotItemStyle,
-            background: i === activeIndex ? '#000' : '#999',
+            background: i === activeIndex ? 'rgba(89,89,89,0.7)' : 'rgba(217,217,217,0.35)',
+            border: '1px solid rgba(40,40,40,0.7)',
             transform: `scale(${i === activeIndex ? 1.3 : 1})`,
           }}
         ></span>
