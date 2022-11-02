95ab7e5d92d58ce122897aeeb3efb6344f122760
dominikjasek <dominik.jasek@customink.com>
2022-11-02 07:49:19 +0100

    jwt expires in 30years

--- apps/strapi/config/plugins.ts
+++ apps/strapi/config/plugins.ts
@@ -1,4 +1,12 @@
-module.exports = ({env}) => ({
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwt: {
+        expiresIn: '30y',
+      },
+    }
+  },
+
   email: {
     config: {
       provider: 'strapi-provider-email-sendinblue',
@@ -192,11 +200,11 @@ module.exports = ({env}) => ({
         // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html
         heading: {
           options: [
-            {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
-            {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
-            {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
-            {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
-            {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'},
+            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
           ]
         },
         // https://ckeditor.com/docs/ckeditor5/latest/features/general-html-support.html
