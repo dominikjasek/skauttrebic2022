5b82738468e463fc853ca9ace6327090e4f824ab
dominikjasek <dominik.jasek@customink.com>
2022-11-03 21:07:15 +0100

    allow public users to fetch posts

--- apps/strapi/src/api/post/controllers/post.ts
+++ apps/strapi/src/api/post/controllers/post.ts
@@ -4,4 +4,33 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::post.post');
+export default factories.createCoreController('api::post.post', ({ strapi }) => ({
+  async find(ctx) {
+    const jwt = ctx.request.header.authorization?.split(' ')[1]
+    const verify = strapi.service('plugin::users-permissions.jwt').verify
+
+    const isAuthorized = jwt ? await verify(jwt) : false
+
+    // Calling the default core action
+    // eslint-disable-next-line prefer-const
+    let { data, meta } = await super.find(ctx);
+
+    if (!isAuthorized) {
+      data = data.map(post => {
+        if (post.attributes.public) return post
+        return {
+          ...post,
+          id: post.id,
+          attributes: {
+            ...post.attributes,
+            title: null,
+            content: null,
+            files: null
+          }
+        }
+      })
+    }
+
+    return { data, meta };
+  },
+}));
