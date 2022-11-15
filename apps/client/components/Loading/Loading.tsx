5ae921b85f1494c40383b7591cff1d20fb2e4980
dominikjasek <dominik.jasek@customink.com>
2022-11-15 11:06:56 +0100

    loading component

--- apps/client/components/Loading/Loading.tsx
+++ apps/client/components/Loading/Loading.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { CircularProgress, Box, Stack } from '@mui/material'
 
 interface LoadingProps {
 }
 
 export const Loading: React.FC<LoadingProps> = () => {
   return (
-    <div>Načítám data...</div>
+    <Stack sx={{ justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+      <CircularProgress />
+    </Stack>
   )
 }
