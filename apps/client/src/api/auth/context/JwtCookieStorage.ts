d323201c25314009cece414e4236eb3abcf18815
dominikjasek <dominik.jasek@customink.com>
2023-01-09 21:35:34 +0100

    expires is in days

--- apps/client/src/api/auth/context/JwtCookieStorage.ts
+++ apps/client/src/api/auth/context/JwtCookieStorage.ts
@@ -1,10 +1,11 @@
 import Cookie from 'js-cookie'
 
 const JWT_KEY = 'jwt'
+const EXPIRES_IN = 1825 // days
 
 export class JwtCookieStorage {
   set(value: string) {
-    Cookie.set(JWT_KEY, value, { expires: 2147483647 })
+    Cookie.set(JWT_KEY, value, { expires: EXPIRES_IN })
   }
 
   get() {
