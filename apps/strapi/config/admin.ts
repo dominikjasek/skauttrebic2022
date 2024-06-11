8a8a4bd22e9ad55de1b48cfeb054c9f1ea888c81
dominikjasek <dominik.jasek@customink.com>
2024-06-11 08:25:08 +0200

    increase expiration token to 2 years

--- apps/strapi/config/admin.ts
+++ apps/strapi/config/admin.ts
@@ -1,6 +1,9 @@
-export default ({env}) => ({
+export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    options: {
+      expiresIn: '2y'
+    }
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
