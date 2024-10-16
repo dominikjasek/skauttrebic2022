7e81ca6d94215a590bf6002434852f1b4a1cb162
dominikjasek <dominik.jasek@customink.com>
2024-10-16 12:03:28 +0200

    move script to body

--- apps/client/pages/_document.tsx
+++ apps/client/pages/_document.tsx
@@ -10,11 +10,11 @@ export default class MyDocument extends Document {
           <meta name="description" content="2. katolické oddíly Třebíč" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
           <link rel="icon" type="image/png" href="/skaut_logo.png" />
-          <Script defer src="https://umami.dominikjasek.cz/script.js" data-website-id="3a0f4766-a62b-4ae8-ba21-ab8e21c06ce9" />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <Script defer src="https://umami.dominikjasek.cz/script.js" data-website-id="3a0f4766-a62b-4ae8-ba21-ab8e21c06ce9" />
         </body>
       </Html>
     )
