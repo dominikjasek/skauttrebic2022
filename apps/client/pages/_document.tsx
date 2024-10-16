72c487bc132ee8b288a9bc1262d1cfd49d0666eb
dominikjasek <dominik.jasek@customink.com>
2024-10-16 11:46:06 +0200

    use umami instead of vercel

--- apps/client/pages/_document.tsx
+++ apps/client/pages/_document.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
@@ -9,6 +10,7 @@ export default class MyDocument extends Document {
           <meta name="description" content="2. katolické oddíly Třebíč" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
           <link rel="icon" type="image/png" href="/skaut_logo.png" />
+          <Script defer src="https://umami.dominikjasek.cz/script.js" data-website-id="3a0f4766-a62b-4ae8-ba21-ab8e21c06ce9" />
         </Head>
         <body>
           <Main />
