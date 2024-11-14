215ba8d52b4876f2f6648713270b22d4f4486105
dominikjasek <dominik.jasek@customink.com>
2024-11-14 11:59:54 +0100

    fix error

--- apps/client/components/Avatar/PersonAvatar.tsx
+++ apps/client/components/Avatar/PersonAvatar.tsx
@@ -30,7 +30,7 @@ function stringAvatar(name: string) {
     sx: {
       bgcolor: stringToColor(name),
     },
-    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+    children: name.includes(' ') ? `${name.split(' ')[0][0]}${name.split(' ')[1][0]}` : name,
     alt: name
   }
 }
