1b3cc3125d901742fb0139a27dc49073dac246ff
Jiří Schreiber <jiri.i.schreiber@gmail.com>
2026-01-14 18:25:03 +0100

    Hide nonmain phones properly

--- apps/client/src/api/contacts/ContactsRepository.tsx
+++ apps/client/src/api/contacts/ContactsRepository.tsx
@@ -1,23 +1,59 @@
 import { GraphQLClient } from 'graphql-request'
 import { useGraphqlRequestClient } from '~/src/api/lib/graphqlRequestClient'
 import {
-  GetContactCardsDocument,
-  GetTroopContactCardsDocument
+  GetContactCardsDocument
 } from '~/src/api/gql/graphql'
+import { IApiCall, useApiCall } from '~/src/api/lib/apiCall'
+
+export interface ContactCard {
+  id: number
+  name: string
+  nickname: string
+  role: string
+  phone: string
+  email: string
+  about: string
+  photo: {
+    data: {
+      attributes: {
+        url: string
+      }
+    }
+  }
+}
+
+export interface Troop {
+  id: number
+  name: string
+  contactCards: ContactCard[]
+}
+
+export interface TroopContactCards {
+  data: {
+    attributes: {
+      troop: Troop[]
+    }
+  }
+}
 
 class ContactsRepository {
-  constructor(private readonly graphqlRequestClient: GraphQLClient) {}
+  constructor(
+    private readonly fetch: IApiCall,
+    private readonly graphqlRequestClient: GraphQLClient
+  ) {}
 
   fetchContactsData = async () => {
     return await this.graphqlRequestClient.request(GetContactCardsDocument)
   }
 
   fetchTroopContactCards = async () => {
-    return await this.graphqlRequestClient.request(GetTroopContactCardsDocument)
+    return (await this.fetch('/troop-contact/?populate=deep')).data as TroopContactCards
   }
 }
 
 export const useContactsRepository = () => {
+  const apiCall = useApiCall()
   const graphqlClient = useGraphqlRequestClient()
-  return new ContactsRepository(graphqlClient)
+
+  return new ContactsRepository(apiCall, graphqlClient)
 }
