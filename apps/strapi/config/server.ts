f073adf31115f4bfb84fedd6870062413a641b02
Dominik Jašek <dominik@dominikjasek.cz>
2026-01-14 13:44:35 +0100

    fix strapi forgot password url by providing url of server as env

--- apps/strapi/config/server.ts
+++ apps/strapi/config/server.ts
@@ -3,6 +3,7 @@ import cronTasks from './functions/cron'
 export default ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', `http://localhost:${env.int('PORT', 1337)}`),
   app: {
     keys: env.array('APP_KEYS'),
   },
