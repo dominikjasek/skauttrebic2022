e25ce90fdc0574efe6d2113b2d481edc318d95b8
Jiří Schreiber <jiri.i.schreiber@gmail.com>
2026-01-19 15:11:36 +0100

    Use new name and logo

--- apps/client/pages/_document.tsx
+++ apps/client/pages/_document.tsx
@@ -5,10 +5,10 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="cs">
-        <Head title={'Skaut Třebíč'}>
+        <Head title={'Skautské středisko Rikitan'}>
           <meta name="description" content="2. katolické oddíly Třebíč" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-          <link rel="icon" type="image/png" href="/skaut_logo.png" />
+          <link rel="icon" type="image/png" href="/rikitan_logo.png" />
           <script defer src="https://umami.dominikjasek.cz/script.js" data-website-id="3a0f4766-a62b-4ae8-ba21-ab8e21c06ce9"></script>
         </Head>
         <body>
